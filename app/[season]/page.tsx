21c205ca34e22465e422585afb9ac443f46453ce
Agustín Risso <agustin.risso@renaiss.io>
2026-01-24 19:28:12 -0300

    feat: display race schedule with cards and results link

--- app/[season]/page.tsx
+++ app/[season]/page.tsx
@@ -1,3 +1,8 @@
+import { RaceCardModel } from "@/lib/f1-types"
+import { getSchedule } from "@/lib/f1-api"
+import { formatRaceDate } from "@/lib/date-helpers"
+import Link from "next/link"
+
 type PageProps = {
     params: Promise<{
         season: string
@@ -7,7 +12,21 @@ type PageProps = {
 export default async function Page({ params }: PageProps) {
     const { season } = await params
 
+    const schedule = await getSchedule(season)
+
     return (
-        <div>Schedule - Season {season}</div>
+        <div className="mx-auto max-w-5xl px-4 py-6 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+            {schedule.map((race: RaceCardModel) => (
+                <div className="w-full flex flex-col rounded-lg bg-gray-800 p-4">
+                    <h2>Round {race.round}: {race.title}</h2>
+                    <p>{race.circuit}</p>
+                    <p>{race.locality}, {race.country}</p>
+                    <p>{formatRaceDate(race.date, race.time)}</p>
+                    {race.date < new Date().toISOString() && (
+                        <Link className="self-end underline cursor-pointer" href={"race-details"}>See race results {'>'}</Link>
+                    )}
+                </div>
+            ))}
+        </div>
     )
 }
